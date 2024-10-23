1b290f89b1b0ece4bc4a436cdc2cf3c26bbd7d54
Khang <khangng2001@gmail.com>
2024-10-23 16:38:05 +0700

    update player movement

---
+++ WeilionAmong/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    #region Fields
+        private Vector2 _playerMovement;
+    #endregion
+    
+    public Vector2 PlayerMovement => _playerMovement;
+    private PlayerControls _playerControls;
+
+    private void Awake()
+    {
+        _playerControls = new PlayerControls();
+    }
+    
+    private void OnEnable()
+    {
+        _playerControls.Player.Movement.performed += OnMoving;
+        _playerControls.Player.Movement.canceled += OnStopMoving;
+        _playerControls.Enable();
+    }
+    private void OnDisable()
+    {
+        _playerControls.Player.Movement.performed -= OnMoving;
+        _playerControls.Player.Movement.canceled -= OnStopMoving;
+        _playerControls.Disable();
+    }
+
+    private void OnStopMoving(InputAction.CallbackContext callback)
+    {
+        _playerMovement = Vector2.zero;
+    }
+
+
+    private void OnMoving(InputAction.CallbackContext callback)
+    {
+        _playerMovement = callback.ReadValue<Vector2>();
+    }
+}
---
+++ WeilionAmong/Assets/Scripts/MovementController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementController : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 3f;
+
+    private InputHandler _inputHandler;
+    private Rigidbody2D _rigidbody2D;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _inputHandler = GetComponent<InputHandler>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move(_inputHandler.PlayerMovement);
+    }
+
+    private void Move(Vector2 direction)
+    {
+        Vector2 moveDir = direction.normalized * _moveSpeed;
+        _rigidbody2D.velocity = moveDir;
+
+    }
+}
